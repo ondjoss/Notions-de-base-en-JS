dd56ed0cbb84cd687292989a5d642c856a8f5efb
ondjoss <ghislain.chakam@gmail.com>
2022-06-22 17:11:48 +0200

    Affichage de la réponse par le clique. Injection de l'événement par une classe

--- dom.js
+++ dom.js
@@ -1,7 +1,23 @@
 //Gestion des events en JS
 
 const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
 
 questionContainer.addEventListener('click', () =>{
     questionContainer.classList.toggle("question-clicked");
 });
+
+btn1.addEventListener('click', () =>{
+    response.classList.add("show-response");
+    response.style.background = "green";
+});
+
+btn2.addEventListener('click', () =>{
+    response.classList.add("show-response");
+    response.style.background = "red";
+});
+
+//---------------------------------------------------------
